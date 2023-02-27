ab0750e55912eb0861bc022678a61560d3aaded9
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2023-02-27 16:58:25 +0000

    Brands done. Time 1h38min

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,28 @@
 import React from "react";
 import "./brand.css";
 
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
+
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      <div className="">
+        <img src={google} alt="Logo Google" />
+      </div>
+      <div className="">
+        <img src={slack} alt="Logo Slack" />
+      </div>
+      <div className="">
+        <img src={atlassian} alt="Logo Atlassian" />
+      </div>
+      <div className="">
+        <img src={dropbox} alt="Logo Dropbox" />
+      </div>
+      <div className="">
+        <img src={shopify} alt="Logo Shopify" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
